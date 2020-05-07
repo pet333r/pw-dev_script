939abbca257f835ff1b122e12b96516f305b2a24
pet333r <piotr.wos.developer@gmail.com>
2020-05-07 17:18:38 +0200

    fix JF-17 caution / Ka-50 PVI-800

--- Modules/Ka-50.lua
+++ Modules/Ka-50.lua
@@ -3,6 +3,21 @@ ExportScript.FoundDCSModule = true
 
 ExportScript.ConfigEveryFrameArguments = 
 {
+    [315] = "%0.1f", -- Waypoints LED
+    [519] = "%0.1f", -- Inflight INU Realignment LED
+    [316] = "%0.1f", -- Fixpoints LED
+    [520] = "%0.1f", -- Precise INU Alignment LED
+    [317] = "%0.1f", -- Airfields LED
+    [521] = "%0.1f", -- Normal INU Alignment LED
+    [318] = "%0.1f", -- Targets LED
+    [313] = "%0.1f", -- Enter LED
+    [314] = "%0.1f", -- Cancel LED
+    [522] = "%0.1f", -- Initial Nav Pos LED
+    [319] = "%0.1f", -- Self coordinates LED
+    [320] = "%0.1f", -- Course:Course Deviation/Time/Range to WPT LED
+    [321] = "%0.1f", -- Wind Heading/Speed LED
+    [322] = "%0.1f", -- True Heading/Time/Range to final WPT LED
+    [323] = "%0.1f", -- Bearing/Range to target LED
 }
 
 ExportScript.ConfigArguments = 
@@ -13,4 +28,16 @@ function ExportScript.ProcessDCSConfigHighImportance(mainPanelDevice)
 end
 
 function ExportScript.ProcessDCSConfigLowImportance(mainPanelDevice)
+    -- PVI-800 Navigation Control Panel 
+    local indPVI = ExportScript.Tools.getListIndicatorValue(5)
+
+    local pviLine1 = coerce_nil_to_string(indPVI.txt_VIT)
+    local pviLine2 = coerce_nil_to_string(indPVI.txt_NIT)
+    local pviLine1Point = coerce_nil_to_string(indPVI.txt_OIT_PPM)
+    local pviLine2Point = coerce_nil_to_string(indPVI.txt_OIT_NOT)
+	ExportScript.Tools.SendData(2008, pviLine1)
+    ExportScript.Tools.SendData(2009, pviLine2)
+    ExportScript.Tools.SendData(2010, pviLine1Point)
+    ExportScript.Tools.SendData(2011, pviLine2Point)
+
 end
