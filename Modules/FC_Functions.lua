0726224d0c44437a3a9a24d406a8fab78579f98d
pet333r <piotr.wos.developer@gmail.com>
2021-07-09 14:52:29 +0200

    commit

--- Modules/FC_Functions.lua
+++ Modules/FC_Functions.lua
@@ -426,51 +426,43 @@ function ExportScript.AF.SPO15RWR_SendData(lKey, lValue)
 end
 
 function ExportScript.AF.StatusLamp()
-	-- local lMCPState = LoGetMCPState() -- Warning Lights
-	-- if lMCPState == nil then
-	-- 	return
-	-- end
+	local lMCPState = LoGetMCPState() -- Warning Lights
+	if lMCPState == nil then
+		return
+	end
 
+	ExportScript.Tools.SendData("702", lMCPState.MasterWarning == true and 1 or 0 )
+	ExportScript.Tools.SendData("711", lMCPState.EOSFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("712", lMCPState.ECMFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("713", lMCPState.CannonFailure == true and 1 or 0 )
 
-	-- ExportScript.Tools.SendData("702", lMCPState.MasterWarning == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("711", lMCPState.EOSFailure == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("712", lMCPState.ECMFailure == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("713", lMCPState.CannonFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("714", lMCPState.MLWSFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("715", lMCPState.ACSFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("716", lMCPState.RadarFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("717", lMCPState.HelmetFailure == true and 1 or 0 )
 
-	-- ExportScript.Tools.SendData("714", lMCPState.MLWSFailure == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("715", lMCPState.ACSFailure == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("716", lMCPState.RadarFailure == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("717", lMCPState.HelmetFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("718", lMCPState.HUDFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("719", lMCPState.MFDFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("720", lMCPState.RWSFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("721", lMCPState.GearFailure == true and 1 or 0 )
 
-	-- ExportScript.Tools.SendData("718", lMCPState.HUDFailure == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("719", lMCPState.MFDFailure == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("720", lMCPState.RWSFailure == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("721", lMCPState.GearFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("722", lMCPState.HydraulicsFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("723", lMCPState.AutopilotFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("724", lMCPState.FuelTankDamage == true and 1 or 0 )
+	ExportScript.Tools.SendData("725", lMCPState.CanopyOpen == true and 1 or 0 )
 
-	-- ExportScript.Tools.SendData("722", lMCPState.HydraulicsFailure == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("723", lMCPState.AutopilotFailure == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("724", lMCPState.FuelTankDamage == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("725", lMCPState.CanopyOpen == true and 1 or 0 )
+	ExportScript.Tools.SendData("726", lMCPState.StallSignalization == true and 1 or 0 )
+	ExportScript.Tools.SendData("727", lMCPState.AutopilotOn == true and 1 or 0 )
+	ExportScript.Tools.SendData("709", lMCPState.LeftWingPumpFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("710", lMCPState.RightWingPumpFailure == true and 1 or 0)
 
-	-- ExportScript.Tools.SendData("726", lMCPState.StallSignalization == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("727", lMCPState.AutopilotOn == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("709", lMCPState.LeftWingPumpFailure == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("710", lMCPState.RightWingPumpFailure == true and 1 or 0)
+	ExportScript.Tools.SendData("700", lMCPState.LeftTailPlaneFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("701", lMCPState.RightTailPlaneFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("703", lMCPState.LeftEngineFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("704", lMCPState.RightEngineFailure == true and 1 or 0 )
 
-	-- ExportScript.Tools.SendData("700", lMCPState.LeftTailPlaneFailure == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("701", lMCPState.RightTailPlaneFailure == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("703", lMCPState.LeftEngineFailure == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("704", lMCPState.RightEngineFailure == true and 1 or 0 )
-
-	-- ExportScript.Tools.SendData("705", lMCPState.LeftAileronFailure == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("706", lMCPState.RightAileronFailure == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("707", lMCPState.LeftMainPumpFailure == true and 1 or 0 )
-	-- ExportScript.Tools.SendData("708", lMCPState.RightMainPumpFailure == true and 1 or 0 )
-
-
-	-- local lAccelerationUnits = LoGetAccelerationUnits()
-	-- if lAccelerationUnits ~= nil then
-	-- 	--ExportScript.Tools.WriteToLog('lAccelerationUnits: '..ExportScript.Tools.dump(lAccelerationUnits))
-	-- 	ExportScript.Tools.SendDataDAC("732", (lAccelerationUnits.y > 8.0 and 1 or 0) ) -- lamp Over-G warning
-	-- end
+	ExportScript.Tools.SendData("705", lMCPState.LeftAileronFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("706", lMCPState.RightAileronFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("707", lMCPState.LeftMainPumpFailure == true and 1 or 0 )
+	ExportScript.Tools.SendData("708", lMCPState.RightMainPumpFailure == true and 1 or 0 )
 end
