de31ac70148fecd826e8e1363178632f65a81921
pet333r <piotr.wos.developer@gmail.com>
2022-01-12 15:11:56 +0100

    2022.01.14

--- .update/update.ps1
+++ .update/update.ps1
@@ -1,67 +1,39 @@
 Set-ExecutionPolicy RemoteSigned
 
+$urlMain = "https://raw.githubusercontent.com/pet333r/pw-dev_script/master/"
+$urlUpdate = "$urlMain.update/"
+
+# sciezka do folderu (folder ze skryptem power shell)
+$dirMain = "$PSScriptRoot\"
+
+# url: list of files to download
+$urlFilesList =  -Join($urlUpdate, "update_script")
+# url: list of additional files to download
+$urlAdditionalList = -Join($urlUpdate, "update_root")
+# pobranie pliku i zapis zawartosci jako lista
+$listFiles = (Invoke-webrequest -URI $urlFilesList ).Content -split "`n"
+$listAddFiles = (Invoke-webrequest -URI $urlAdditionalList ).Content -split "`n"
+
+
 $separator = "*******************************************************"
 Write-Host "$separator`n**************** Updating pw-dev_script ***************`n"
 
 $start_time = Get-Date
 
-# checking lib directory
+# checking directories
 $path = "lib"
-If(!(test-path $path))
-{
-      New-Item -ItemType Directory -Force -Path $path
-}
+If(!(test-path $path)) { New-Item -ItemType Directory -Force -Path $path }
 
-# checking Modules directory
 $path = "Modules"
-If(!(test-path $path))
-{
-      New-Item -ItemType Directory -Force -Path $path
-}
+If(!(test-path $path)) { New-Item -ItemType Directory -Force -Path $path }
 
-# checking Utilities directory
 $path = "Utilities"
-If(!(test-path $path))
-{
-      New-Item -ItemType Directory -Force -Path $path
-}
+If(!(test-path $path)) { New-Item -ItemType Directory -Force -Path $path }
 
-$urlMain = "https://raw.githubusercontent.com/pet333r/pw-dev_script/master/"
-$urlUpdate = "https://raw.githubusercontent.com/pet333r/pw-dev_script/master/.update/"
-$urlLib = $urlMain + "lib/"
-$urlMod = $urlMain + "Modules/"
-$urlUti = $urlMain + "Utilities/"
 
-# folders dir
-$dirMain = "$PSScriptRoot\"
-$dirLib = "$PSScriptRoot\lib\"
-$dirModules = "$PSScriptRoot\Modules\"
-$dirUtilities = "$PSScriptRoot\Utilities\"
-
-# files main
-$listFilesMain = @(
-      'add_firewall_rules.bat'
-      'Config.lua'
-      'ExportInit.lua'
-      'my_local_PC_IP.cmd'
-      'version'
-)
-
-# modules
-$urlModFile =  -Join($urlUpdate, "update_modules")
-$modulesList = (Invoke-webrequest -URI $urlModFile ).Content -split "`n"
-
-# libs
-$urlLibFile =  -Join($urlUpdate, "update_lib")
-$libList = (Invoke-webrequest -URI $urlLibFile ).Content -split "`n"
-
-# utilities
-$urlUtilFile =  -Join($urlUpdate, "update_utils")
-$utilList = (Invoke-webrequest -URI $urlUtilFile ).Content -split "`n"
-
-# download main files
-Write-Host "Trying to download configuration files: $dirMain"
-foreach ($file in $listFilesMain) {
+# download files in folders
+Write-Host "`nTrying to download files:"
+foreach ($file in $listFiles) {
       If($file -eq  "Config.lua")
       {
             # if file doesn't exist > download, else ignore
@@ -78,36 +50,8 @@ foreach ($file in $listFilesMain) {
             $url = $urlMain + $file
             $output = $dirMain + $file
             Invoke-WebRequest -Uri $url -OutFile $output
-            Write-Host "Downloading:" $file
+            Write-Host "Downloading: $file"
       }
-}
-
-# download folder 'lib'
-Write-Host "`nTrying to download files to folder: $dirLib"
-foreach ($file in $libList) {
-      $url = $urlLib + $file
-      $output = $dirLib + $file
-      Invoke-WebRequest -Uri $url -OutFile $output
-      Write-Host "Downloading:" $file
-}
-
-# download folder 'Modules'
-Write-Host "`nTrying to download modules to: $dirModules"
-foreach ($module in $modulesList) {
-      $url = $urlMod + $module
-      $output = $dirModules + $module
-      #Write-Host "Downloading:" $module
-      Invoke-WebRequest -Uri $url -OutFile $output
-      Write-Host "Downloading:" $module
-}
-
-# download folder 'Utilities'
-Write-Host "`nTrying to download files to folder: $dirUtilities"
-foreach ($file in $utilList) {
-      $url = $urlUti + $file
-      $output = $dirUtilities + $file
-      Invoke-WebRequest -Uri $url -OutFile $output
-      Write-Host "Downloading:" $file
 }
 
 #--------------------------------------------------------------------------
