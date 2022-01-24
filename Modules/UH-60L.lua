0879c38d2eb6e797e3092a70cc8aab1297a4c0e6
pet333r <piotr.wos.developer@gmail.com>
2022-01-24 21:44:19 +0100

    init UH-60L

---
+++ Modules/UH-60L.lua
@@ -0,0 +1,17 @@
+-- UH-60L Black Hawk Mod
+ExportScript.FoundDCSModule = true
+
+ExportScript.ConfigEveryFrameArguments = 
+{
+}
+
+ExportScript.ConfigArguments = 
+{
+}
+
+
+function ExportScript.ProcessDCSConfigHighImportance(mainPanelDevice)
+end
+
+function ExportScript.ProcessDCSConfigLowImportance(mainPanelDevice)
+end
