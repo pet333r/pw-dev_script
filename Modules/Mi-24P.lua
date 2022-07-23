0f31e2bddaf892ea95d1b76681e01468a35ad3b9
pet333r <piotr.wos.developer@gmail.com>
2022-07-23 20:19:01 +0200

    2022.07.23

--- Modules/Mi-24P.lua
+++ Modules/Mi-24P.lua
@@ -4,32 +4,98 @@ ExportScript.FoundDCSModule = true
 ExportScript.ConfigEveryFrameArguments = 
 {
     -- Pilot cautions
-    [150] = "%.1f", -- 
-    [151] = "%.1f", -- 
-    [152] = "%.1f", -- 
-    [153] = "%.1f", -- 
-    [154] = "%.1f", -- 
-    [155] = "%.1f", -- 
-    [156] = "%.1f", -- 
-    [157] = "%.1f", -- 
-    [158] = "%.1f", -- 
-    [159] = "%.1f", -- 
-    [160] = "%.1f", -- 
-    [161] = "%.1f", -- 
-    [162] = "%.1f", -- 
-    [163] = "%.1f", -- 
-    [164] = "%.1f", -- 
-    [165] = "%.1f", -- 
-    [166] = "%.1f", -- 
-    [167] = "%.1f", -- 
-    [168] = "%.1f", -- 
-    [169] = "%.1f", -- 
-    [170] = "%.1f", -- 
-    [171] = "%.1f", -- 
-    [172] = "%.1f", -- 
-    [175] = "%.1f", -- 
-    [176] = "%.1f", -- 
-    [177] = "%.1f", -- 
+    [11] = "%.1f", -- VERT GYRO 1 FAIL
+    [13] = "%.1f", -- VERT GYRO 2 FAIL
+    [150] = "%.1f", -- ICING
+    [151] = "%.1f", -- FIRE
+    [152] = "%.1f", -- TANK 1 120 LITRES REMAIN
+    [153] = "%.1f", -- TANK 1 120 LITRES REMAIN
+    [154] = "%.1f", -- MAIN G/B LOW OIL PRESS
+    [155] = "%.1f", -- ATTENTION TO ELECT CONT.PANEL
+    [156] = "%.1f", -- ANTI-ICE AUTO TEST
+    [157] = "%.1f", -- LIMIT MANEUVER
+    [158] = "%.1f", -- CHIPS IN GRBX OIL
+    [159] = "%.1f", -- LH ENG HIGH VIBRATION
+    [160] = "%.1f", -- RH ENG HIGH VIBRATION
+    [161] = "%.1f", -- LH ENG HIGH TEMP
+    [162] = "%.1f", -- RH ENG HIGH TEMP
+    [163] = "%.1f", -- M&T ROT ANTI-ICE FAIL
+    [164] = "%.1f", -- HIGH CABIN PRESS
+    [165] = "%.1f", -- HIGH PEDAL PUSH RATE
+    [166] = "%.1f", -- SHUT DOWN LH ENG
+    [167] = "%.1f", -- SHUT DOWN RH ENG
+    [168] = "%.1f", -- CHIPS IN LH ENG OIL
+    [169] = "%.1f", -- CHIPS IN RH ENG OIL
+    [170] = "%.1f", -- SELECT STBY CODE
+    [171] = "%.1f", -- XPDR FAIL
+    [172] = "%.1f", -- HIGH CABIN PRESS
+    [173] = "%.1f", -- SPEED HOLD
+    [174] = "%.1f", -- MAP LIMIT
+    [175] = "%.1f", -- RH ENG DUST PROT ON
+    [176] = "%.1f", -- LH ENG DUST PROT ON
+    [177] = "%.1f", -- CONTROL TO OPERATOR
+
+    -- Signal, flares
+    [343] = "%1d", -- Cassette 1 Power Switch
+    [344] = "%1d", -- Cassette 1 Launch Red Button
+    [345] = "%1d", -- Cassette 1 Launch Green Button
+    [346] = "%1d", -- Cassette 1 Launch Yellow Button
+    [347] = "%1d", -- Cassette 1 Launch White Button
+    [352] = "%1d", -- Cassette 2 Power Switch
+    [351] = "%1d", -- Cassette 2 Launch Red Button
+    [350] = "%1d", -- Cassette 2 Launch Green Button
+    [349] = "%1d", -- Cassette 2 Launch Yellow Button
+    [348] = "%1d", -- Cassette 2 Launch White Button
+
+    -- Radio navigation
+    [365] = "%1d", -- RWR Signal
+    [366] = "%1d", -- RWR Power
+    [367] = "%1d", -- Greben' ON/OFF
+    [368] = "%1d", -- Gyro 2 Power
+    [369] = "%1d", -- Gyro 1 Power
+    [370] = "%1d", -- Airspeed to DISS ON/OFF
+    [371] = "%1d", -- DISS-15D ON/OFF
+    [372] = "%1d", -- RV-5 ON/OFF
+    [373] = "%1d", -- Eucalypt-M24 ON/OFF
+    [374] = "%1d", -- Jadro-1I ON/OFF
+    [375] = "%1d", -- R-863 ON/OFF
+    [376] = "%1d", -- Switch SPU-8 NET-1 ON/OFF
+    [377] = "%1d", -- Switch SPU-8 NET-2 ON/OFF
+
+    -- HEATIN
+    [388] = "%.1f", -- LEFT HEAT FAIL
+    [390] = "%.1f", -- RIGHT HEAT FAIL
+    [391] = "%.1f", -- LH TANK 1 VALVE CLOSED
+    [393] = "%.1f", -- RH TANK 2 VALVE CLOSED
+    [395] = "%.1f", -- LH ENG VALVE CLOSED
+    [398] = "%.1f", -- RH ENG VALVE CLOSED
+    [401] = "%.1f", -- ISOLATING VALVE CLOSED
+    [403] = "%.1f", -- PUMP N4 ON
+    [405] = "%.1f", -- PUMP N5 ON
+    [407] = "%.1f", -- PUMP N1 ON
+    [409] = "%.1f", -- PUMP N2 ON
+    [412] = "%.1f", -- CUT OFF TRANSF.
+
+    -- FIRE PROTECTION SYSTEM
+    [470] = "%.1f", -- LEFT ENGINE FIRE
+    [471] = "%.1f", -- LEFT ENGINE FIRE 1
+    [472] = "%.1f", -- LEFT ENGINE FIRE 2
+    [473] = "%.1f", -- RIGHT ENGINE FIRE
+    [474] = "%.1f", -- RIGHT ENGINE FIRE 1
+    [475] = "%.1f", -- RIGHT ENGINE FIRE 2
+    [476] = "%.1f", -- APU TANK 3 CMPTS FIRE
+    [477] = "%.1f", -- APU TANK 3 CMPTS FIRE 1
+    [478] = "%.1f", -- APU TANK 3 CMPTS FIRE 2
+    [479] = "%.1f", -- MAIN GEAR BOX CMPT FIRE
+    [480] = "%.1f", -- MAIN GEAR BOX CMPT FIRE 1
+    [481] = "%.1f", -- MAIN GEAR BOX CMPT FIRE 2
+    [483] = "%.1f", -- FIRE DET TEST
+
+    [559] = "%.1f", -- USLP
+    [560] = "%.1f", -- FXD MG
+    [561] = "%.1f", -- BOMBS
+    [562] = "%.1f", -- 30
+    [563] = "%.1f", -- RKT
 }
 
 ExportScript.ConfigArguments = 
---
+++ Modules/Mirage-F1CE.lua
@@ -0,0 +1,20 @@
+-- Module Name Export
+ExportScript.FoundDCSModule = true
+
+ExportScript.ConfigEveryFrameArguments = 
+{
+}
+
+ExportScript.ConfigArguments = 
+{
+}
+
+-- Pointed to by ProcessDCSHighImportance
+function ExportScript.ProcessDCSConfigHighImportance(mainPanelDevice)
+
+end
+
+-- Pointed to by ExportScript.ProcessDCSConfigLowImportance
+function ExportScript.ProcessDCSConfigLowImportance(mainPanelDevice)
+
+end
