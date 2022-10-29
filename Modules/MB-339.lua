0d37048255c968172ac8252ff6bf210d764812d0
pet333r <pet333r@PET333R-ASUS>
2022-10-30 01:04:38 +0200

    2022.10.30

---
+++ Modules/MB-339.lua
@@ -0,0 +1,16 @@
+-- MB-339
+ExportScript.FoundDCSModule = true
+
+ExportScript.ConfigEveryFrameArguments = 
+{
+}
+
+ExportScript.ConfigArguments = 
+{
+}
+
+function ExportScript.ProcessDCSConfigHighImportance(mainPanelDevice)
+end
+
+function ExportScript.ProcessDCSConfigLowImportance(mainPanelDevice)
+end
