d363ecb74b0c99b88bdca8e3f8753a830e7b6c44
pet333r <piotr.wos.developer@gmail.com>
2023-06-22 19:48:00 +0200

    2023.06.22

---
+++ Modules/F-15ESE.lua
@@ -0,0 +1,19 @@
+-- F-15E Strike Eagle
+
+ExportScript.FoundDCSModule = true
+
+ExportScript.ConfigEveryFrameArguments = 
+{
+}
+
+ExportScript.ConfigArguments = 
+{
+}
+
+function ExportScript.ProcessDCSConfigHighImportance(mainPanelDevice)
+
+end
+
+function ExportScript.ProcessDCSConfigLowImportance(mainPanelDevice)
+
+end
