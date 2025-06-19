ad00630238d84e65cce631b2ad4a02a117b722ef
pet333r <piotr.wos.developer@gmail.com>
2025-06-19 22:13:11 +0200

    2025.06.19

---
+++ Modules/F4U-1D.lua
@@ -0,0 +1,16 @@
+-- F4U-1D
+PWDEV.FoundDCSModule = true
+
+PWDEV.ConfigEveryFrameArguments =
+{
+}
+
+PWDEV.ConfigArguments =
+{
+}
+
+function PWDEV.ProcessDCSConfigHighImportance(mainPanelDevice)
+end
+
+function PWDEV.ProcessDCSConfigLowImportance(mainPanelDevice)
+end
